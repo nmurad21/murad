0d0620ba7251a53515adcaa9ea831d21d3c561e5
tu7kr926y <tu7kr926y@214-11>
2022-03-04 08:28:26 +0400

    Add project files.

---
+++ murad/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace murad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string month = Console.ReadLine() ;
+            if (month == "january" || month == "february" || month == "december")
+            {
+                Console.WriteLine("winter");
+            }
+            else if (month == "march" || month == "april" || month == "may")
+            {
+                Console.WriteLine("spring");
+            }
+            else if (month == "june" || month == "july" || month == "august")
+            {
+                Console.WriteLine("summer");
+            }
+            else if (month=="september" || month=="october" || month == "november")
+            {
+                Console.WriteLine("autumn");
+            }
+                   
+        }
+    }
+}
